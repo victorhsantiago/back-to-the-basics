92907f61231eb58b5eed8e23547d07cdf941fafd
Victor Santiago <victorhsantiago3@gmail.com>
2022-03-27 21:10:28 -0300

    feat: implement tasks manament logic :wink::wink:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return
+
+    setTasks([
+      ...tasks,
+      {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }
+    ])
+    
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updatedTasks = tasks.map(task => (
+      task.id === id
+        ? { ...task, isComplete: !task.isComplete }
+        : task
+    ))
+
+    setTasks(updatedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const updatedTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(updatedTasks)
   }
 
   return (
@@ -34,7 +53,8 @@ export function TaskList() {
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar novo todo" 
+            placeholder="Adicionar novo todo"
+            required
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
